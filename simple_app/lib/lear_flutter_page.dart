ed50d3ae7d41caea7c47ec3a62c56611c8ca4f84
IbnuEyni <shuaibahmedin@gmail.com>
2024-08-06 17:41:42 +0300

    first final app

---
+++ simple_app/lib/lear_flutter_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool isCheck = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn Flutter'),
+        backgroundColor: Colors.green,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Action');
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/original.jpeg'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black12,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is text widget',
+                  style: TextStyle(color: Colors.amber),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: isSwitch ? Colors.green : Colors.grey),
+              onPressed: () {
+                debugPrint('Elevated Button');
+              },
+              child: const Text('Elevated Button'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlined Button');
+              },
+              child: const Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('Text Button');
+              },
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('This is the row');
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('This is row widget'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(
+                  () {
+                    isSwitch = newBool;
+                  },
+                );
+              },
+            ),
+            Checkbox(
+              value: isCheck,
+              onChanged: (bool? newBool) {
+                setState(
+                  () {
+                    isCheck = newBool!;
+                  },
+                );
+              },
+            ),
+            Image.network(
+                "https://mrwallpaper.com/images/hd/cool-art-albert-einstein-neon-abstract-gsifeeeti198lcjo.jpg"),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- simple_app/lib/main.dart
+++ simple_app/lib/main.dart
@@ -1,4 +1,8 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:simple_app/home_page.dart';
+import 'package:simple_app/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: RootPage(),
     );
   }
@@ -23,8 +28,42 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  List<Widget> pages = const [HomePage(), ProfilePage()];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter'),
+        backgroundColor: Colors.green,
+      ),
+      body: pages[currentPage],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Floating action Button');
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'profile',
+          )
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
   }
 }
